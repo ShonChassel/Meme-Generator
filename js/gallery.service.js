c3ef3f8c302ee8ad1c5325791a2b1c1777e762c0
ShonChassel <gershoni1234@gmail.com>
2022-10-27 02:23:27 +0300

    img chang

--- js/gallery.service.js
+++ js/gallery.service.js
@@ -6,16 +6,16 @@ var gImgs = []
 
 
 
-var gImg1 = `./css/img/1.jpg`
-var gImg2 = `./css/img/2.jpg`
-var gImg3 = `./css/img/3.jpg`
-var gImg4 = `./css/img/4.jpg`
-var gImg5 = `./css/img/5.jpg`
-var gImg6 = `./css/img/6.jpg`
-var gImg7 = `./css/img/7.jpg`
-var gImg8 = `./css/img/8.jpg`
-var gImg9 = `./css/img/9.jpg`
-var gImg10 = `./css/img/10.jpg`
+var gImg1 = `css/img/1.jpg`
+var gImg2 = `css/img/2.jpg`
+var gImg3 = `css/img/3.jpg`
+var gImg4 = `css/img/4.jpg`
+var gImg5 = `css/img/5.jpg`
+var gImg6 = `css/img/6.jpg`
+var gImg7 = `css/img/7.jpg`
+var gImg8 = `css/img/8.jpg`
+var gImg9 = `css/img/9.jpg`
+var gImg10 = `css/img/10.jpg`
 const gImages = [gImg1, gImg2, gImg3, gImg4, gImg5, gImg6, gImg7, gImg8, gImg9, gImg10]
 
 
